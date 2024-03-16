f40be0bc94513253361f937ac91ac68535d0213d
tjstone1 <theojstone@yahoo.com>
2024-03-16 18:35:02 +0000

    Adds human score selection and dice locking

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,57 @@
 const button = document.getElementById("roll-btn");
+const scoreCard = document.getElementById("score-card");
+const scoreRows = scoreCard.children[0].children;
+let cells = [];
+
+function disposeUnselected(unselected) {}
+function handleSelect(e) {
+  if (!player) return;
+  const target = cells.find((cell) => {
+    if (cell === e.target) {
+      return cell;
+    }
+  });
+
+  if (!target.innerText) {
+    target.innerText = 0;
+    clearPreviews(previewValues);
+    previewValues = [];
+    return;
+  } else if (!previewValues.includes(target)) {
+    return;
+  } else {
+    const unselected = previewValues.filter((cell) => {
+      if (cell !== target) {
+        return cell;
+      }
+    });
+    clearPreviews(unselected);
+  }
+  previewValues = [];
+}
+for (let i = 0; i < 17; i++) {
+  if (i === 0 || i === 7 || i === 8 || i === 16) {
+    continue;
+  }
+  const cell = scoreRows[i].children[1];
+  cell.addEventListener("click", (e) => handleSelect(e));
+  cells.push(cell);
+}
 const diceContainer = document.getElementById("dice-container");
 const diceElements = Array.from(diceContainer.children);
+let player = true;
+
+function toggleTurn() {
+  player = !player;
+}
 
 function toggleLock(index) {
   dice[index].locked = !dice[index].locked;
+}
+
+function handleToggleLock(index) {
+  if (!player) return;
+  toggleLock(index);
 }
 
 function isConsecutive(scores) {
@@ -18,6 +66,33 @@ function isConsecutive(scores) {
 
 function sum(scores) {
   return scores.reduce((a, b) => a + b, 0);
+}
+
+let previewValues = [];
+function clearPreviews(previewValues) {
+  console.log(previewValues);
+  for (const value of previewValues) {
+    value.innerText = "";
+  }
+  previewValues = [];
+}
+
+function toggleClickable() {
+  console.log("done");
+}
+
+function showAvailableMoves(scores) {
+  clearPreviews(scores);
+  console.log(scoreRows);
+  let i = 1;
+  let col = player ? 1 : 2;
+  for (const score of scores) {
+    if (!scoreRows[i].cells[col].innerText && score > 0) {
+      previewValues.push(scoreRows[i].cells[col]);
+      scoreRows[i].cells[col].innerText = score;
+    }
+    i++;
+  }
 }
 
 function checkScores(scores) {
@@ -59,9 +134,10 @@ function checkScores(scores) {
         break;
       case 3:
         threeOAK = sum(scores);
+        fullHouse = 25;
         break;
       case 2:
-        fullHouse = 0;
+        fullHouse = 25;
         threeOAK = uniqueValues[1] * 3;
         break;
       case 1:
@@ -90,16 +166,39 @@ function checkScores(scores) {
   const orderedScores = uniqueValues.sort();
 
   smallStraight =
-    isConsecutive(orderedScores.slice(0, 4)) ||
-    isConsecutive(orderedScores.slice(1, 5));
-  largeStraight = isConsecutive(orderedScores);
+    isConsecutive(scores.sort().slice(0, 4)) ||
+    isConsecutive(scores.sort().slice(1, 5))
+      ? 30
+      : 0;
+  largeStraight = isConsecutive(scores.sort()) ? 40 : 0;
+  const allScores = [
+    ...upperScores,
+    0,
+    0,
+    threeOAK,
+    fourOAK,
+    fullHouse,
+    smallStraight,
+    largeStraight,
+    total,
+    yahtzee,
+  ];
+  console.log(allScores);
+  showAvailableMoves(allScores);
+}
 
-  console.log(threeOAK, fourOAK, fullHouse, yahtzee);
+function handleRoll() {
+  if (!player) return;
+  roll();
 }
 
 function roll() {
   const scores = [];
   dice.forEach((die) => {
+    if (die.locked) {
+      die.locked = false;
+      return;
+    }
     const value = Math.ceil(Math.random() * 6);
     die.value = value;
     die.element.innerText = value;
@@ -109,7 +208,7 @@ function roll() {
 }
 
 diceElements.forEach((die, index) =>
-  die.addEventListener("click", () => toggleLock(index))
+  die.addEventListener("click", () => handleToggleLock(index))
 );
 
 let dice = diceElements.map((die, index) => ({
@@ -118,4 +217,4 @@ let dice = diceElements.map((die, index) => ({
   locked: false,
 }));
 
-button.addEventListener("click", roll);
+button.addEventListener("click", handleRoll);
